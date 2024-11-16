ff8ae50f206e104bc8ec4525aa4baa560366667d
JOYSARKER763 <it23022@mbstu.ac.bd>
2024-11-16 12:46:37 +0600

    Add files via upload
    exercise 1

---
+++ Ch1Ex1.java
@@ -0,0 +1,7 @@
+public class Ch1Ex1{
+public static void main(String args[]){
+System.out.println("Programing is fun!");
+System.out.println("Fundamentals First");
+System.out.println("problem Driven");
+}
+}
