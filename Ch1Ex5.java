6ff8e14c9476169e540baaaf59c077d790d4e73b
JOYSARKER763 <it23022@mbstu.ac.bd>
2024-11-17 07:22:07 +0600

    exercise 5

---
+++ Ch1Ex5.java
@@ -0,0 +1,13 @@
+public class Ch1Ex5{
+public static void main(String args[]){
+
+System.out.println("a    a^2    a^3");
+int i;
+for( i=1;i<=4;i++)
+{
+
+System.out.println(i+"     "+i*i+"      "+i*i*i);
+
+}
+}
+}
