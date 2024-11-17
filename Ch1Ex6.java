776ceee351a6ec35bb0e18f9f4afce3d5737434b
JOYSARKER763 <it23022@mbstu.ac.bd>
2024-11-17 18:28:57 +0600

    Ch1Ex6.java

--- Ch1Ex6.java
+++ Ch1Ex6.java
@@ -1,3 +1,5 @@
+
+//make a simple program in java which can do print a simple mathematical calculation
 public class Ch1Ex6{
 public static void main(String args[]){
 System.out.println((9.5*4.5-2.5*3)/(45.5-3.5));
