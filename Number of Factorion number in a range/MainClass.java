ca5881c7b69351d9039c2068f7381bc601bb4d37
JOYSARKER763 <it23022@mbstu.ac.bd>
2024-12-06 14:02:47 +0600

    Add files via upload

--- Number of Factorion number in a range/MainClass.java
+++ Number of Factorion number in a range/MainClass.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
+public class MainClass {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the lower bound of the range: ");
+        int lowerBound = scanner.nextInt();
+
+        System.out.print("Enter the upper bound of the range: ");
+        int upperBound = scanner.nextInt();
+
+        System.out.println("Factorion numbers in the range:");
+        for (int i = lowerBound; i <= upperBound; i++) {
+            if (FactorionChecker.isFactorion(i)) {
+                System.out.println(i);
+            }
+        }
+
+        scanner.close();
+    }
+}
 
