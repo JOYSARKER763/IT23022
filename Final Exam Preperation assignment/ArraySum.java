ac128d4a2b8359ffcd20a564650412c7a3535ce9
JOYSARKER763 <it23022@mbstu.ac.bd>
2025-02-15 11:31:01 +0600

    ArraySum.java

--- Final Exam Preperation assignment/ArraySum.java
+++ Final Exam Preperation assignment/ArraySum.java
@@ -1,25 +1,25 @@
 public class ArraySum {
 
-    // Method to calculate the sum of all elements in an integer array
+    
     public static int calculateSum(int[] arr) {
-        int sum = 0;  // Variable to store the sum of array elements
+        int sum = 0;  
 
-        // Loop through the array and add each element to sum
+        
         for (int num : arr) {
             sum += num;
         }
 
-        return sum;  // Return the calculated sum
+        return sum;  
     }
 
     public static void main(String[] args) {
-        // Defining an array of integers
+      
         int[] numbers = {10, 20, 30, 40, 50};
 
-        // Calling the calculateSum method and storing the result
+        
         int sum = calculateSum(numbers);
 
-        // Displaying the sum of array elements
+        
         System.out.println("The sum of the array elements is: " + sum);
     }
 }
