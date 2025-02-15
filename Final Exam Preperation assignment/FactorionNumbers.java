9e80dd200517ed7e0fce7f24f4352200bc6802de
JOYSARKER763 <it23022@mbstu.ac.bd>
2025-02-15 11:34:28 +0600

    Update FactorionNumbers.java

--- Final Exam Preperation assignment/FactorionNumbers.java
+++ Final Exam Preperation assignment/FactorionNumbers.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class FactorionNumbers {
 
-    // Precompute the factorials of digits 0-9
+  
     static int[] factorials = new int[10];
 
     static {
@@ -11,7 +11,7 @@ public class FactorionNumbers {
         }
     }
 
-    // Method to calculate factorial of a number
+    
     static int factorial(int n) {
         int fact = 1;
         for (int i = 1; i <= n; i++) {
@@ -20,7 +20,7 @@ public class FactorionNumbers {
         return fact;
     }
 
-    // Method to check if a number is a factorion
+   
     static boolean isFactorion(int number) {
         int originalNumber = number;
         int sumOfFactorials = 0;
@@ -37,14 +37,14 @@ public class FactorionNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Take user input for lower and upper bounds
+       
         System.out.print("Enter the lower bound: ");
         int lowerBound = scanner.nextInt();
 
         System.out.print("Enter the upper bound: ");
         int upperBound = scanner.nextInt();
 
-        // Print factorion numbers in the given range
+        
         System.out.println("Factorion numbers between " + lowerBound + " and " + upperBound + ":");
         for (int i = lowerBound; i <= upperBound; i++) {
             if (isFactorion(i)) {
